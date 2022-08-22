60a20cd4805472bc61cab40ba22d4f3ce45de040
DenD <dendanyleiko@gmail.com>
2022-08-22 15:39:00 +0300

    Добавьте файлы проекта.

---
+++ PizzaOrder/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaOrder.Models;
+using System.Diagnostics;
+
+namespace PizzaOrder.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
