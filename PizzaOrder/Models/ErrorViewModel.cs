2e8ed3f0967f34e04d7ba63f14642d445e4af2ca
DenD <dendanyleiko@gmail.com>
2022-08-22 18:28:52 +0300

    Adding Pizza & PizzaComponent models

--- PizzaOrder/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PizzaOrder.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ PizzaOrder/Models/Pizza.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaOrder.Models
+{
+    public class Pizza
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Required]
+        public float Price { get; set; }
+        public ICollection<PizzaComponent>? PizzaComponents { get; set; }
+    }
+}
