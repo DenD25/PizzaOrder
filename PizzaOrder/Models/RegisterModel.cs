7043b28039a309af2295f25657afd70e9bc4c1f8
DenD <dendanyleiko@gmail.com>
2022-08-23 11:18:25 +0300

    Adding register model

---
+++ PizzaOrder/Models/RegisterModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaOrder.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Login is required")]
+        public string? Login { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string? ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required")]
+        [DataType(DataType.PhoneNumber)]
+        public int PhoneNumber { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+        [Required(ErrorMessage = "Field is required")]
+        public string? StreetName { get; set; }
+        [Required(ErrorMessage = "Field is required")]
+        public string? HouseNumber { get; set; }
+        public int ApartmentsNumber { get; set; }
+    }
+}
