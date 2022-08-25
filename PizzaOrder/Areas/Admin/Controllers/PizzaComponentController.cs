cb8afe6a9b17acb28e6ca938f26a215ff3cc0466
DenD <dendanyleiko@gmail.com>
2022-08-25 16:21:32 +0300

    PizzaComponent adding

---
+++ PizzaOrder/Areas/Admin/Controllers/PizzaComponentController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PizzaOrder.Models;
+
+namespace PizzaOrder.Areas.Admin.Controllers
+{
+    
+    public class PizzaComponentController : Controller
+    {
+        ApplicationContext db;
+
+        public PizzaComponentController(ApplicationContext context)
+        {
+            db = context;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PizzaComAdd()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PizzaComAdd(PizzaComponent pizzaComponent)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PizzaComponents.Add(pizzaComponent);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(pizzaComponent);
+            }
+        }
+    }
+}
