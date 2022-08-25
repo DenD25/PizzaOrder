2a87233df7f67808033d91bbbda9e5695cc17f97
DenD <dendanyleiko@gmail.com>
2022-08-25 19:50:50 +0300

    Adding Pizza controller, list & create

---
+++ PizzaOrder/Areas/Admin/Controllers/PizzaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PizzaOrder.Models;
+
+namespace PizzaOrder.Areas.Admin.Controllers
+{
+    public class PizzaController : Controller
+    {
+        ApplicationContext db;
+
+        public PizzaController(ApplicationContext context)
+        {
+            db = context;
+
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await db.PizzaComponents.ToListAsync());
+        }
+    }
+}
