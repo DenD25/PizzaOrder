ce016ae7ceedd1f72c06b870cb2080770843ceaa
DenD <dendanyleiko@gmail.com>
2022-08-27 13:47:19 +0300

    Renaming controller

---
+++ PizzaOrder/Areas/User/Controllers/MainController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PizzaOrder.Areas.User.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
