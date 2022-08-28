d12a6a012ca0fc774d69edfba24f9344b659b859
DenD <dendanyleiko@gmail.com>
2022-08-28 12:50:37 +0300

    Adding profile controller

---
+++ PizzaOrder/Areas/User/Controllers/ProfileController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PizzaOrder.Models;
+
+namespace PizzaOrder.Areas.User.Controllers
+{
+    public class ProfileController : Controller
+    {
+        ApplicationContext db;
+
+        public ProfileController(ApplicationContext context)
+        {
+            db = context;
+        }
+        public async Task<IActionResult> Index(int id)
+        {
+             return  View(await db.Users.FirstOrDefaultAsync(x => x.Id == id));
+        }
+    }
+}
