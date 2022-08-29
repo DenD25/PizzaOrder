228ec1bba0b6d308249169e7d6cbad01fb025162
DenD <dendanyleiko@gmail.com>
2022-08-29 19:34:47 +0300

    Creating OrderUser view model

---
+++ PizzaOrder/ViewModel/OrderUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using PizzaOrder.Models;
+
+namespace PizzaOrder.ViewModel
+{
+    public class OrderUserViewModel
+    {
+        public OrderUser OrderUser { get; set; }
+        public IEnumerable<Pizza> Pizzas { get; set; }
+        public IEnumerable<PizzaComponent> PizzaComponents { get; set; }
+    }
+}
