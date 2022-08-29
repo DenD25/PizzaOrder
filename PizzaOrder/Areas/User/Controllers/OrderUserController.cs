277982a382be5a0e57c8d98be2cc795ababa1bac
DenD <dendanyleiko@gmail.com>
2022-08-29 21:21:24 +0300

    Creating OrderUser controller

---
+++ PizzaOrder/Areas/User/Controllers/OrderUserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PizzaOrder.Models;
+using PizzaOrder.ViewModel;
+
+namespace PizzaOrder.Areas.User.Controllers
+{
+    public class OrderUserController : Controller
+    {
+        ApplicationContext db;
+
+        public OrderUserController(ApplicationContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            List<Pizza> pizzas = db.Pizzas.ToList();
+            List<PizzaComponent> pizzaComponents = db.PizzaComponents.ToList();
+            OrderUser order = new OrderUser();
+
+            OrderUserViewModel ouvm = new OrderUserViewModel { OrderUser = order, PizzaComponents = pizzaComponents, Pizzas = pizzas };
+
+            return View(ouvm);
+        }
+    }
+}
