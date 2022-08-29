1e272bade4ebdd09299814f7333d7e22f935baf8
DenD <dendanyleiko@gmail.com>
2022-08-29 12:45:40 +0300

    Adding Order model for Anonymous

---
+++ PizzaOrder/Models/OrderAnonymous.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaOrder.Models
+{
+    public class OrderAnonymous
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Phone number is required")]
+        [DataType(DataType.PhoneNumber)]
+        public int PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Field is required")]
+        public string? City { get; set; }
+        [Required(ErrorMessage = "Field is required")]
+        public string? StreetName { get; set; }
+        [Required(ErrorMessage = "Field is required")]
+        public string? HouseNumber { get; set; }
+        public string? ApartmentsNumber { get; set; }
+        public string? PostCode { get; set; }
+        [Required(ErrorMessage = "Please, write your name.")]
+        [StringLength(30)]
+        public string? Name { get; set; }
+        public ICollection<Pizza>? pizzas { get; set; }
+        public DateTime CreateTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public bool IsDone { get; set; } = false;
+    }
+}
