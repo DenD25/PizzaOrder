90d7fa0834afa2d5a7f9d7f0b44b3c3df3a24bbb
DenD <dendanyleiko@gmail.com>
2022-08-29 12:39:42 +0300

    Adding Order model for users

---
+++ PizzaOrder/Models/OrderUser.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaOrder.Models
+{
+    public class OrderUser
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        public User? user { get; set; }
+        [Required(ErrorMessage = "Please, write your name.")]
+        [StringLength(30)]
+        public string? Name { get; set; }
+        public ICollection<Pizza>? pizzas { get; set; }
+        public DateTime CreateTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public bool IsDone { get; set; } = false;
+    }
+}
