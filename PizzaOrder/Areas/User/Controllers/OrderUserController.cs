3a88275655185b4e6b97da2627982c37363dae9f
DenD <dendanyleiko@gmail.com>
2022-09-08 17:19:56 +0300

    Adding Pizzas to OrderUser

--- PizzaOrder/Areas/User/Controllers/OrderUserController.cs
+++ PizzaOrder/Areas/User/Controllers/OrderUserController.cs
@@ -23,5 +23,66 @@ namespace PizzaOrder.Areas.User.Controllers
 
             return View(ouvm);
         }
+
+        public async Task<IActionResult> AddingPizzas(int id, int count, int pizzaId)
+        {
+            OrderUser orderUser = db
+                .OrderUsers
+                .Include(x => x.Pizzas)
+                .Include(x => x.User)
+                .FirstOrDefault(x => x.UserId == id);
+
+            if (orderUser == null || orderUser.IsOrdered == true)
+            {
+                OrderUser newOrder = new OrderUser();
+
+                newOrder.UserId = id;
+                newOrder.User = db
+                    .Users
+                    .FirstOrDefault(x => x.Id == id);
+                newOrder.Name = "None";                
+                newOrder.CreateTime = DateTime.Now;
+
+                Pizza orderPizza = db
+                    .Pizzas
+                    .FirstOrDefault(x => x.Id == pizzaId);
+
+                List<Pizza> pizzaList = new List<Pizza>();
+
+                orderPizza.Count = count;
+
+                pizzaList.Add(orderPizza);               
+
+                newOrder.Pizzas = pizzaList;
+
+                db.OrderUsers.Add(newOrder);
+                await db.SaveChangesAsync();
+
+                return View(newOrder);
+            }
+
+            else if(orderUser.IsOrdered == false)
+            {
+                Pizza orderPizza = db
+                    .Pizzas
+                    .FirstOrDefault(x => x.Id == pizzaId);
+
+                List<Pizza> pizzaList = orderUser.Pizzas.ToList();
+
+                orderPizza.Count = count;
+
+                pizzaList.Add(orderPizza);
+
+                orderUser.Pizzas = pizzaList;   
+
+                db.OrderUsers.Update(orderUser);
+                await db.SaveChangesAsync();
+
+                return View(orderUser);
+            }
+
+            return RedirectToAction("Index", "OrderUser");
+        }
+
     }
 }
