0e65130aedd46e44a856ea1d1c6b2f8a4ae7d71c
DenD <dendanyleiko@gmail.com>
2022-09-08 19:07:00 +0300

    Creating Menu controller for User role

---
+++ PizzaOrder/Areas/User/Controllers/MenuUserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PizzaOrder.Models;
+
+namespace PizzaOrder.Areas.User.Controllers
+{
+    public class MenuUserController : Controller
+    {
+        ApplicationContext db;
+
+        public MenuUserController(ApplicationContext context)
+        {
+            db = context;
+        }
+
+        public IActionResult MenuUser()
+        {
+            var pizza = db
+                .Pizzas
+                .Include(x => x.PizzaComponents)
+                .ToList();
+
+            return View(pizza);
+        }
+    }
+}
