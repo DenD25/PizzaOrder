c08d2da70b1085969138913c37c4164e8a7dc9c1
DenD <dendanyleiko@gmail.com>
2022-09-12 21:56:43 +0300

    Adding OrderList controller

---
+++ PizzaOrder/Areas/Manager/Controllers/OrderManager.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaOrder.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace PizzaOrder.Areas.Manager.Controllers
+{
+    public class OrderManager : Controller
+    {
+        ApplicationContext db;
+
+        public OrderManager(ApplicationContext context)
+        {
+            db = context;
+        }
+        public IActionResult OrderList()
+        {
+            List<Order> order = db
+                .OrderUsers
+                .Where(x => x.IsOrdered == true)
+                .Include(x => x.Pizzas)
+                .ToList();
+                
+            return View(order);
+        }
+    }
+}
