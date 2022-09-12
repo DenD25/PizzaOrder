1d78a6260e1d6349dde07768ba83cd151f066c70
DenD <dendanyleiko@gmail.com>
2022-09-12 21:54:58 +0300

    UserId is not required more

--- PizzaOrder/Models/Order.cs
+++ PizzaOrder/Models/Order.cs
@@ -6,7 +6,6 @@ namespace PizzaOrder.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required]
         public int? UserId { get; set; }
         public User? User { get; set; }
         public string? Name { get; set; }
