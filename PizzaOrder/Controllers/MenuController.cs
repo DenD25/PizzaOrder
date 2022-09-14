aabbb26b946ae0548241bbe03c69bfbe6d05a3ac
DenD <dendanyleiko@gmail.com>
2022-09-14 15:56:00 +0300

    Adding Menu controller

---
+++ PizzaOrder/Controllers/MenuController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PizzaOrder.Models;
+
+namespace PizzaOrder.Controllers
+{
+    public class MenuController : Controller
+    {
+        ApplicationContext db;
+
+        public MenuController(ApplicationContext context)
+        {
+            db = context;
+        }
+
+        public IActionResult Menu()
+        {
+            List<Pizza> pizzas = db
+                .Pizzas
+                .Include(x => x.PizzaComponents)
+                .ToList();
+
+            return View(pizzas);
+        }
+    }
+}
