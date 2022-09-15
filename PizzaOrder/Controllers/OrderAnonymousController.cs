d41327d32e00952287d31e6ab3f4832da3789cd9
DenD <dendanyleiko@gmail.com>
2022-09-15 17:57:42 +0300

    Creating OrderAnonymous controller

---
+++ PizzaOrder/Controllers/OrderAnonymousController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PizzaOrder.Models;
+using PizzaOrder.ViewModel;
+
+namespace PizzaOrder.Controllers
+{
+    public class OrderAnonymousController : Controller
+    {
+        ApplicationContext db;
+
+        public OrderAnonymousController(ApplicationContext context)
+        {
+            db = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
