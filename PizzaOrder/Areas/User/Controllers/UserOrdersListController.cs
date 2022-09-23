f31e697e77c5ce52456eedecff6811b8dc7a295f
DenD <dendanyleiko@gmail.com>
2022-09-23 20:59:03 +0300

    Adding new controller for user order list

---
+++ PizzaOrder/Areas/User/Controllers/UserOrdersListController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PizzaOrder.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PizzaOrder.Models;
+
+
+namespace PizzaOrder.Areas.User.Controllers
+{
+    public class UserOrdersListController : Controller
+    {
+        ApplicationContext db;
+
+        public UserOrdersListController(ApplicationContext context)
+        {
+            db = context;
+        }
+
+        public IActionResult Orders(int id)
+        {
+            List<Order> order = db
+                .OrderUsers
+                .Where(x => x.IsOrdered == true)
+                .Where(x => x.UserId == id)
+                .Include(x => x.Pizzas)
+                .ToList();
+
+            return View(order);
+        }
+    }
+}
