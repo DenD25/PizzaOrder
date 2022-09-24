be10531e1d174af6e126ba0131b017f5585452ba
DenD <dendanyleiko@gmail.com>
2022-09-24 17:42:52 +0300

    Adding Delivering bool for Order model

--- PizzaOrder/Models/Order.cs
+++ PizzaOrder/Models/Order.cs
@@ -12,6 +12,7 @@ namespace PizzaOrder.Models
         public ICollection<Pizza>? Pizzas { get; set; }
         public DateTime? CreateTime { get; set; }
         public DateTime? EndTime { get; set; }
+        public bool? Delivering { get; set; }
         public bool? IsOrdered { get; set; } = false;
         public bool? IsCooked { get; set; } = false;
         public bool? IsDelivered { get; set; } = false;
