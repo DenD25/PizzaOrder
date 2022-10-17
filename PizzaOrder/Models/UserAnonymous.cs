5514a812be175799e4dba0d547dd17fb51c69703
DenD <dendanyleiko@gmail.com>
2022-10-17 23:52:59 +0300

    Creating model for anonymous users

---
+++ PizzaOrder/Models/UserAnonymous.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaOrder.Models
+{
+    public class UserAnonymous
+    {
+        [Required(ErrorMessage = "Please, write your name.")]
+        [StringLength(30)]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Phone number is required")]
+        [DataType(DataType.PhoneNumber)]
+        public int PhoneNumber { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+        [Required(ErrorMessage = "Field is required")]
+        public string? City { get; set; }
+        [Required(ErrorMessage = "Field is required")]
+        public string? StreetName { get; set; }
+        [Required(ErrorMessage = "Field is required")]
+        public string? HouseNumber { get; set; }
+        public string? ApartmentsNumber { get; set; }
+        public string? PostCode { get; set; }
+    }
+}
